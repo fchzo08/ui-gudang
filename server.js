d25ea55178239884b84095523993b05efcb3e2cf
fchzo08 <89438930+fchzo08@users.noreply.github.com>
2024-05-25 05:29:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
